cb755fd824620f0852a12a1b2e1539d38a203210
aminakbari7 <aminakbari733@gmail.com>
2023-12-04 10:33:09 +0330

    Create info.blade.php

---
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -0,0 +1,11 @@
+{{-- user info and avatar --}}
+<div class="avatar av-l chatify-d-flex"></div>
+
+<div class="messenger-infoView-btns">
+    <a href="#" class="danger delete-conversation">پاک کردن گفت و گو</a>
+</div>
+{{-- shared photos --}}
+<div class="messenger-infoView-shared">
+    <p class="messenger-title"><span>تصاویر ارسال شده</span></p>
+    {{-- <div class="shared-photos-list"></div> --}}
+</div>
