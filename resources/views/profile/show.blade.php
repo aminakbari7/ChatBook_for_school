9d73bf107a6cd27ab4dfa651e8ff29e1abf2fd6c
aminakbari7 <aminakbari733@gmail.com>
2023-12-04 10:33:42 +0330

    Create show.blade.php

---
+++ resources/views/profile/show.blade.php
@@ -0,0 +1,34 @@
+<style>
+    *{
+        font-size: 21px !important;
+    }
+</style>
+
+<x-app-layout>
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+                @livewire('profile.update-profile-information-form')
+
+
+            @endif
+
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.update-password-form')
+                </div>
+
+
+            @endif
+
+
+            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+
+
+                <div class="mt-10 sm:mt-0">
+                    @livewire('profile.delete-user-form')
+                </div>
+            @endif
+        </div>
+    </div>
+</x-app-layout>
