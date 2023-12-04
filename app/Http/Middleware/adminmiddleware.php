63bd81af1b1051c7986000f8e4dd2573d6d88b40
aminakbari7 <aminakbari733@gmail.com>
2023-12-04 10:40:43 +0330

    Create adminmiddleware.php

---
+++ app/Http/Middleware/adminmiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+class adminmiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $role = Auth::user()->role;
+        if ($role == 'admin')
+               return $next($request);
+        else
+        return abort(404);
+
+    }
+}
